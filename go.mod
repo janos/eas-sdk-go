cdd36e85d3c22350b903cf1e7e3cccaf86789a22
Janos Guljas <janos@resenje.org>
2024-03-22 02:34:18 +0100

    Improve Schema tests

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.22
 
 require (
 	github.com/ethereum/go-ethereum v1.13.14
-	resenje.org/taint v0.1.3
+	resenje.org/taint v0.1.4
 )
 
 require (
--- schema_test.go
+++ schema_test.go
@@ -17,8 +17,9 @@ import (
 
 func TestSchema(t *testing.T) {
 	type Record struct {
-		Key   eas.UID
-		Value []byte `abi:"val"`
+		Key        eas.UID
+		Value      []byte   `abi:"val"`
+		ABigNumber *big.Int `abi:"abn"`
 	}
 
 	type NestedSliceWithTuples struct {
@@ -26,54 +27,56 @@ func TestSchema(t *testing.T) {
 		Records []Record
 	}
 
-	t.Run("address", newTest[common.Address]("address", common.Address{1, 2, 3}))
-	t.Run("string", newTest[string]("string", "Message"))
-	t.Run("bool", newTest[bool]("bool", true))
-	t.Run("bytes32", newTest[[32]byte]("bytes32", [32]byte{4, 5}))
-	t.Run("uid", newTest[eas.UID]("bytes32", eas.UID{6, 7}))
-	t.Run("bytes", newTest[[]byte]("bytes", []byte{25, 25, 45}))
-	t.Run("uint8", newTest[uint8]("uint8", uint8(2)))
-	t.Run("uint16", newTest[uint16]("uint16", uint16(2)))
-	t.Run("uint32", newTest[uint32]("uint32", uint32(2)))
-	t.Run("uint64", newTest[uint64]("uint64", uint64(2)))
-	t.Run("uint256", newTest[*big.Int]("uint256", big.NewInt(44)))
+	t.Run("address", newSchemaTest("address", common.Address{1, 2, 3}))
+	t.Run("string", newSchemaTest("string", "Message"))
+	t.Run("bool", newSchemaTest("bool", true))
+	t.Run("bytes32", newSchemaTest("bytes32", [32]byte{4, 5}))
+	t.Run("uid", newSchemaTest("bytes32", eas.UID{6, 7}))
+	t.Run("bytes", newSchemaTest("bytes", []byte{25, 25, 45}))
+	t.Run("uint8", newSchemaTest("uint8", uint8(2)))
+	t.Run("uint16", newSchemaTest("uint16", uint16(2)))
+	t.Run("uint32", newSchemaTest("uint32", uint32(2)))
+	t.Run("uint64", newSchemaTest("uint64", uint64(2)))
+	t.Run("uint256", newSchemaTest("uint256", big.NewInt(44)))
 
 	t.Run("nested slice of tuples",
-		newTest[NestedSliceWithTuples]("string msg, (bytes32 Key, bytes val)[] Records", NestedSliceWithTuples{
+		newSchemaTest[NestedSliceWithTuples]("string msg, (bytes32 Key, bytes val, uint256 abn)[] Records", NestedSliceWithTuples{
 			Message: "Hello, 世界",
 			Records: []Record{
 				{
-					Key:   eas.HexDecodeUID("0xb4d0ab81afc3474119212c28a8303ae693510a13d4024dae15eae99a59e2aa7c"),
-					Value: []byte{1, 2, 3, 4, 5, 6, 7},
+					Key:        eas.HexDecodeUID("0xb4d0ab81afc3474119212c28a8303ae693510a13d4024dae15eae99a59e2aa7c"),
+					Value:      []byte{1, 2, 3, 4, 5, 6, 7},
+					ABigNumber: big.NewInt(100),
 				},
 				{
-					Key:   eas.HexDecodeUID("0x2d65177371a8c15a75480b2dedd127d52d9ba9f2aa78bf52886e102a6a76333d"),
-					Value: []byte{0, 0, 1, 0, 0},
+					Key:        eas.HexDecodeUID("0x2d65177371a8c15a75480b2dedd127d52d9ba9f2aa78bf52886e102a6a76333d"),
+					Value:      []byte{0, 0, 1, 0, 0},
+					ABigNumber: big.NewInt(520),
 				},
 			},
 		}),
 	)
 
-	t.Run("slice", newTest[[]string]("string[]", []string{"1", "2"}))
+	t.Run("slice", newSchemaTest("string[]", []string{"1", "2"}))
 
-	t.Run("array", newTest[[2]string]("string[2]", [2]string{"4", "5"}))
+	t.Run("array", newSchemaTest("string[2]", [2]string{"4", "5"}))
 
 	t.Run("nested slices and arrays",
-		newTest[[3][][2]string]("string[2][][3]", [3][][2]string{
-			[][2]string{},
-			[][2]string{
-				[2]string{"", "ab"},
-				[2]string{"cd", "efg"},
-				[2]string{"a", ""},
+		newSchemaTest("string[2][][3]", [3][][2]string{
+			{},
+			{
+				{"", "ab"},
+				{"cd", "efg"},
+				{"a", ""},
 			},
-			[][2]string{
-				[2]string{"one", "two"},
+			{
+				{"one", "two"},
 			},
 		}),
 	)
 }
 
-func newTest[T any](wantSchema string, wantAttestation T) func(*testing.T) {
+func newSchemaTest[T any](wantSchema string, wantAttestation T) func(*testing.T) {
 	return func(t *testing.T) {
 		client := newClient(t)
 		ctx := context.Background()
